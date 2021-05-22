c58136de5f0c25518a286817c49aa2cd43c39d65
5aitama <alexisgougaut@gmail.com>
2021-05-22 14:25:04 +0200

    Add the VertexBuffer class and util function for DataView

---
+++ engine/Core/DataViewUtils.ts
@@ -0,0 +1,39 @@
+import { DataType } from "../types";
+import TypedData from "./TypedData";
+
+/**
+ * Set a `TypedData` into a `DataView`.
+ * @param offset The data offset *(in bytes)*
+ * @param data The data to set.
+ * @param view The `DataView`.
+ * @returns The new offset *(in bytes)* according to the data size.
+ */
+export function setTypedData(offset: number, data: TypedData, view: DataView) {
+    switch(data.type) {
+        case DataType.u8: 
+            view.setUint8(offset, data.value); 
+            break;
+        case DataType.u16: 
+            view.setUint16(offset, data.value, true); 
+            break;
+        case DataType.u32: 
+            view.setUint32(offset, data.value, true); 
+            break;
+
+        case DataType.i8: 
+            view.setInt8(offset, data.value); 
+            break;
+        case DataType.i16: 
+            view.setInt16(offset, data.value, true); 
+            break;
+        case DataType.i32: 
+            view.setInt32(offset, data.value, true);
+            break;
+
+        case DataType.f32: 
+            view.setFloat32(offset, data.value, true); 
+            break;
+    }
+
+    return offset + data.size;
+}
---
+++ engine/Graphics/VertexBuffer.ts
@@ -0,0 +1,105 @@
+import { setTypedData } from "../Core/DataViewUtils";
+import { BufferTarget, BufferUsage } from "../GPUBuffer";
+import { DataType } from "../types";
+import Buffer from "./Buffer";
+import { Vertex } from "./Vertex";
+
+/**
+ * Class that help us to transform
+ * a `Vertex` to a `Buffer`.
+ */
+export default class VertexBuffer<T extends Vertex> {
+    /** The vertices. */
+    private _vertices: T[];
+
+    /** The context. */
+    private _gl: WebGLRenderingContext;
+
+    /** The buffer. */
+    private _buffer: Buffer;
+
+    constructor(gl: WebGLRenderingContext, vertices: T[], usage: BufferUsage) {
+        this._gl = gl;
+        this._vertices = vertices;
+        this._buffer = new Buffer(BufferTarget.Array, usage);
+
+        this._buffer.initBuffer(this._gl, this.inlineData());
+    }
+
+    /**
+     * Update the buffer data. **This function
+     * update the entire buffer so be carefull
+     * when you call this.**
+     */
+    updateBufferData(): void;
+
+    /**
+     * Updates a part of the buffer according to 
+     * the vertex that was modified.
+     * @param index The index of the vertex that was modified.
+     */
+    updateBufferData(index: number): void;
+
+    /**
+     * Updates several parts of the buffer according 
+     * to the vertices that have been modified.
+     * @param indices The index of the vertices that was modified.
+     */
+    updateBufferData(indices: number[]): void;
+
+    /**
+     * Update the buffer data.
+     */
+    updateBufferData(index?: number | number[]) {
+        if(index !== undefined) {
+            if(Array.isArray(index)) {
+                for(let i = 0; i < index.length; i++) {
+                    this.updateBufferData(index[i]);
+                }
+            } else {
+                const size = this._vertices[0].size;
+                const offset = size * index;
+
+                const bufferArray = new ArrayBuffer(size);
+                const view = new DataView(bufferArray);
+
+                let dataOffset = offset;
+
+                for(let i = 0; i < this._vertices[index].vectors.length; i++) {
+                    for(let j = 0; j < this._vertices[index].vectors[i].data.length; j++) {
+                        dataOffset += setTypedData(dataOffset, this._vertices[index].vectors[i].data[j], view);
+                    }
+                }
+
+                this._buffer.updateBufferData(this._gl, offset, bufferArray);
+            }
+        } else {
+            this._buffer.updateBufferData(this._gl, 0, this.inlineData());
+        }
+    }
+
+    /**
+     * Put all vertex data together into a single array. 
+     * It keep the vertex order.
+     * @returns An array that contains all vertex data.
+     */
+    inlineData() {
+        const bufferArray = new ArrayBuffer(this._vertices[0].size * this._vertices.length);
+        const view = new DataView(bufferArray);
+
+        let offset = 0;
+        
+        for(let i = 0; i < this._vertices.length; i++) {
+            for(let j = 0; j < this._vertices[i].vectors.length; j++) {
+                const dataType = this._vertices[i].vectors[j].type;
+                for(let k = 0; k < this._vertices[i].vectors[j].data.length; k++) {
+                    setTypedData(offset, this._vertices[i].vectors[j].data[k], view);
+                    offset += this._vertices[i].vectors[j].data[k].size;
+                }
+            }
+        }
+
+        return bufferArray;
+    }
+
+}
