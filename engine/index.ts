64924ef09a72cd7043b37303cc99222974f9d76c
サイタマ5aitama <alexisgougaut@gmail.com>
2021-05-24 18:53:36 +0200

    Update the index.ts to test the uniforms functions from the material class

--- engine/index.ts
+++ engine/index.ts
@@ -12,36 +12,45 @@ const canvas = document.querySelector("#canvas") as HTMLCanvasElement;
 
 const renderer = new SceneRenderer(canvas);
 
+let material: Material | null = null;
+
 async function setup() {
-    const vertices = [
-        new Vertex([
-            new Vector2(-0.5, -0.5, DataType.f32), 
-            new Vector4(1, 0, 0, 1, DataType.f32)
-        ]),
+    const vertices: Vertex[] = [];
+    const indices: Indices[] = [];
 
-        new Vertex([
-            new Vector2(-0.5, 0.5, DataType.f32), 
-            new Vector4(0, 1, 0, 1, DataType.f32)
-        ]),
+    const radius = 0.5;
+    const resolution = 60;
+    const steps = 360 / resolution;
+    const deg2rad = Math.PI / 180;
 
-        new Vertex([
-            new Vector2(0.5, 0.5, DataType.f32), 
-            new Vector4(0, 0, 1, 1, DataType.f32)
-        ]),
+    // Push the origin vertex of our circle.
+    vertices.push(new Vertex([
+        new Vector2(0, 0, DataType.f32),        // The position.
+        new Vector4(1, 1, 1, 1, DataType.f32),  // The color.
+    ]));
+    
+    for(let i = 0, j = 0; i <= 360; i += steps, j++) {
+        const x = Math.sin(i * deg2rad) * radius;
+        const y = Math.cos(i * deg2rad) * radius;
 
-        new Vertex([
-            new Vector2(0.5, -0.5, DataType.f32), 
-            new Vector4(1, 1, 1, 1, DataType.f32)
-        ]),
-    ];
+        vertices.push(new Vertex([
+            new Vector2(x, y, DataType.f32),        // The position.
+            new Vector4(x, y, 1, 1, DataType.f32),  // The color.
+        ]));
 
-    const indices = [
-        new Indices(new Vector3(0, 1, 2, DataType.u8)),
-        new Indices(new Vector3(0, 2, 3, DataType.u8)),
-    ];
+        if(j === 0) continue;
+
+        indices.push(
+            new Indices(new Vector3(0, j - 1, j, DataType.u8))
+        );
+    }
+
+    indices.push(
+        new Indices(new Vector3(0, resolution, 1, DataType.u8))
+    );
 
     const shader = await Shader.loadFrom("shaders/simple.vert", "shaders/simple.frag");
-    const material = new Material(shader);
+    material = new Material(shader);
     const mesh = new Mesh(vertices, indices, material);
     renderer.addMesh(mesh);
 
@@ -58,7 +67,12 @@ async function setup() {
     }
 }
 
-function render() {
+function render(time = 0) {
+    let x = Math.sin(time / 1000.0) / 2.0;
+    let y = Math.cos(time / 1000.0) / 2.0;
+
+    material!.setUniform("position", new Vector2(x, y, DataType.f32));
+
     renderer.render();
     requestAnimationFrame(render);
 }
--- shaders/simple.vert
+++ shaders/simple.vert
@@ -4,7 +4,9 @@ attribute vec4 vcol; // Vertex color
 
 varying vec4 col;
 
+uniform vec2 position;
+
 void main(void) {
     col = vcol;
-    gl_Position = vec4(vpos, 0, 1);
+    gl_Position = vec4(vpos + position, 0, 1);
 }
